eb1a53742cafcf33ad90a12c32e9a4351423715d
Your Name <you@example.com>
2026-09-18 13:30:45 +0530

    Fix React Router 404 by switching to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
